06f131bd4c2c703660ec3067ddd8f55434b56e71
Ibrahim <imimazim7@gmail.com>
2023-07-16 18:01:14 +0300

    feat: added app modules

---
+++ app_modules.py
@@ -0,0 +1,15 @@
+# ---------------------------------------------------------
+# Application Modules Imports
+# ---------------------------------------------------------
+
+# GUI FILE
+from ui_main import Ui_MainWindow
+
+# QSS Styles
+from ui_styles import Style
+
+# UI Functions
+from ui_functions import *
+
+# Application Logic
+from app_functions import *
